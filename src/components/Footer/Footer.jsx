4f7bfa8bfaded3adea2caf0992a98340e7a0203d
andriiivaniuk <keed888888@gmail.com>
2022-06-19 23:31:56 +0300

    footer quick fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -68,6 +68,17 @@ export function Footer (props){
                 </li>
                 
             </div>
+            <div className='line'></div>
+            <div className="footer-copyright-info">
+                <span className="footer-copyright-item">
+                    Your access to and use of Lorem Ipsum (the app)
+                    is subject exclusively to these Terms and Conditions. You will not use the app for any purpose that is unlawful or prohibited by these Terms and Conditions. By using the app you are fully accepting the terms,
+                    conditions and disclaimers contained in this notice.
+                </span>
+                <span className="footer-copyright-item-right">
+                    Super Cool Website copyright ©
+                </span>
+            </div>
         </footer>
     )
 }
