74009344405e4075333b3dbeb2bf05f68d36528a
andriiivaniuk <keed888888@gmail.com>
2022-06-30 23:06:35 +0300

    add basket logic

--- src/components/Basket/BasketList.jsx
+++ src/components/Basket/BasketList.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import withLoader from "../../hocs/withLoader/withLoader";
 import "./Basket.css"
+import { useEffect } from "react";
 
 export const BasketList = ({products, currentBasket}) => {
 
-    console.log(currentBasket);
-    
     if(!products) {
         return null;
     }
 
+    const [basket, setBasket] = useState(currentBasket);
+    console.log(basket);
+
+    const changeBasketAmount = (id, ifAdd) => {
+        if(!ifAdd && currentBasket[id] == 1) {
+            return;
+        }
+
+        let oldBasket = currentBasket;
+        let newBasket = Object.assign(
+            oldBasket, {
+                [id]: ifAdd ? currentBasket[id] + 1 : currentBasket[id] -1
+            }
+        )
+        
+        setBasket(Object.assign({}, newBasket));
+        localStorage.setItem("basket", JSON.stringify(newBasket));
+    }
+
+    const deleteItem = (id) => {
+        let oldBasket = currentBasket;
+        delete oldBasket[id];
+
+        localStorage.setItem("basket", JSON.stringify(oldBasket));
+        setBasket(Object.assign({}, oldBasket));
+    }
+    
     return(
+        <>{ Object.keys(basket).length === 0 && 
+            
+        <p className="basket-empty-text">Your basket is empty :(</p> } 
+        
         <div className="basket-list">
-            {products.map((product, index) => 
+            {products.map((product, index) =>  basket[product.id] &&
                 <div className="list__item" key = {Math.random()}>
                     <span className="list__num">
                         {index + 1}.
@@ -27,15 +57,15 @@ export const BasketList = ({products, currentBasket}) => {
                         ${product.price}
                     </span>
                     <div className="amount-set">
-                        <button className="lower-amount">-1</button>
+                        <button className="lower-amount" onClick={() => changeBasketAmount(product.id, false)}>-1</button>
                         <span className="list__amount">
-                            x {currentBasket[product.id]}
+                            x {basket[product.id]}
                         </span>
-                        <button  className="upper-amount">+1</button>
-                        <button className="delete-item"> x </button>
+                        <button className="upper-amount" onClick={() => changeBasketAmount(product.id, true)} >+1</button>
+                        <button className="delete-item" onClick={() => deleteItem(product.id)} > x </button>
                     </div>
                     <span className="list__full-price">
-                        ${(product.price * currentBasket[product.id]).toFixed(2)}
+                        ${(product.price * basket[product.id]).toFixed(2)}
                     </span>
             
                 </div>
@@ -43,6 +73,7 @@ export const BasketList = ({products, currentBasket}) => {
 
             <div className="line"></div>
         </div>
+        </>
     )
 }
 
